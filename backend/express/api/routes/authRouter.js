d6d23982ff2d7132f94ee9391867d0a2b5a2a69e
Prashant2002pd <92350732+Prashant2002pd@users.noreply.github.com>
2024-11-10 03:54:03 +0530

    added update quiz route

--- backend/express/api/routes/authRouter.js
+++ backend/express/api/routes/authRouter.js
@@ -8,13 +8,22 @@ import ClassController from "../controllers/group.controllers.js";
 const Router = express.Router();
 
 Router.get("/auth/google", googleAuth);
+
+//group router
 Router.post("/group/", ClassController.createClass);
 Router.get("/group/groups", ClassController.fetchAllClasses);
 Router.put("/class/:groupId/quiz", ClassController.updateQuiz);
-router.put("/class/:groupId/notes", authMiddleware, ClassController.updateNotes);
+Router.put(
+  "/class/:groupId/notes",
+  authMiddleware,
+  ClassController.updateNotes
+);
 Router.put("/class/:groupId/participants", ClassController.updateParticipants);
 Router.get("/group/user/:userId/groups", ClassController.fetchUserClasses);
+
+//quiz router
 Router.post("/quiz/createQuiz", QuizController.createQuiz);
+Router.put("/quiz/updatequiz/:id", QuizController.updateQuiz);
 Router.get("/quiz/:id", QuizController.getQuiz);
 Router.get("/quiz/", QuizController.getAllQuizzes);
 Router.delete("/quiz/del/:id", QuizController.deleteQuiz);
