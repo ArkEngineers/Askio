5f0a4c94fe572ae0b669ca70cfb222effde60a03
= <mridultiwari2002@gmail.com>
2024-11-09 15:55:22 +0530

    all page height

--- frontend/src/Pages/More/MainLayout.jsx
+++ frontend/src/Pages/More/MainLayout.jsx
@@ -4,7 +4,7 @@ import { Outlet } from 'react-router-dom'
 function MainLayout() {
   return (
     <div>
-        <div>
+        <div className='bg-black h-screen text-grey-5'>
             <Outlet/>
         </div>
     </div>
