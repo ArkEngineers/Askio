d9d37433b909c548d98f8e9c40a40c3eac33780d
Azeem Idrisi <azeemidrisi@protonmail.com>
2024-11-10 02:12:53 +0530

    Slug, And Routing Done

--- frontend/src/Components/ClassCard.jsx
+++ frontend/src/Components/ClassCard.jsx
@@ -1,9 +1,29 @@
 import React from "react";
 import { FaUserCircle } from "react-icons/fa";
+import { useAuth } from "../Context/AuthContext";
+import { Link } from "react-router-dom";
 
 function ClassCard({ title, date, faculty }) {
+  const { setModule } = useAuth();
+
+  function stringToSlug(str) {
+    return str
+      .toLowerCase() // Convert to lowercase
+      .trim() // Remove whitespace from start and end
+      .replace(/[^a-z0-9 -]/g, "") // Remove invalid characters
+      .replace(/\s+/g, "-") // Replace spaces with -
+      .replace(/-+/g, "-"); // Replace multiple - with single -
+  }
+  const handleClick = () => {
+    setModule(stringToSlug(title));
+  };
+
   return (
-    <div className="w-60 h-64 min-w-60 hover:bg-gray-800 transition-all duration-300 bg-grey-6 mx-2 rounded-xl flex flex-col overflow-hidden justify-between pb-2">
+    <Link
+      to={`/app/${stringToSlug(title)}/notes`}
+      onClick={handleClick}
+      className="w-60 h-64 min-w-60 hover:bg-gray-800 transition-all duration-300 bg-grey-6 mx-2 rounded-xl flex flex-col overflow-hidden justify-between pb-2"
+    >
       <div className="w-full h-1/2  ">
         <img
           src="https://builtin.com/sites/www.builtin.com/files/2024-10/artificial-intelligence.jpg"
@@ -22,7 +42,7 @@ function ClassCard({ title, date, faculty }) {
           <p className="mx-4 text-xs text-grey-1 w-full text-left">{date}</p>
         </div>
       </div>
-    </div>
+    </Link>
   );
 }
 
--- frontend/src/Components/Sidebar.jsx
+++ frontend/src/Components/Sidebar.jsx
@@ -6,21 +6,19 @@ import { SlPuzzle, SlSettings } from "react-icons/sl";
 import { useLocation, Link } from "react-router-dom";
 
 export function Sidebar() {
-    const location = useLocation();
-    console.log(location.pathname.slice(4))
-    const isActive = (path) => location.pathname === path;
+  const location = useLocation();
+  console.log(location.pathname.slice(4));
+  const isActive = (path) => location.pathname.includes(path);
 
-    return (
-        <div className="fixed left-0 top-20 text-base-3 flex flex-col justify-start items-center text-2xl bg-grey-9 h-screen border-r gap-6 border-gray-800 z-10 h-[calc(100vh-2rem)] p-4 shadow-xl shadow-blue-gray-900/5">
-            <Link to="/app">
-                {isActive("/app") ? <FaHome /> : <FiHome />}
-            </Link>
-            <Link to="/puzzle">
-                {isActive("/puzzle") ? <FaPuzzlePiece /> : <SlPuzzle />}
-            </Link>
-            <Link to="/settings">
-                {isActive("/settings") ? <FcSettings /> : <SlSettings />}
-            </Link>
-        </div>
-    );
+  return (
+    <div className="fixed left-0 top-20 text-base-3 flex flex-col justify-start items-center text-2xl bg-grey-9 h-screen border-r gap-6 border-gray-800 z-10 h-[calc(100vh-2rem)] p-4 shadow-xl shadow-blue-gray-900/5">
+      <Link to="/app">{isActive("/app") ? <FaHome /> : <FiHome />}</Link>
+      <Link to="/puzzle">
+        {isActive("/puzzle") ? <FaPuzzlePiece /> : <SlPuzzle />}
+      </Link>
+      <Link to="/settings">
+        {isActive("/settings") ? <FcSettings /> : <SlSettings />}
+      </Link>
+    </div>
+  );
 }
