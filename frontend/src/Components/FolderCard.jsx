102607a4576d9ddbcb8feb4a53c312494b35363c
Azeem Idrisi <azeemidrisi@protonmail.com>
2024-11-09 22:46:25 +0530

    Hover animation, Take quiz button, Groups row

--- frontend/src/Components/FolderCard.jsx
+++ frontend/src/Components/FolderCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function FolderCard({ title, date }) {
   return (
-    <div className="w-40 h-40 min-w-40 bg-grey-6 mx-2 rounded-xl flex flex-col overflow-hidden justify-between pb-2">
+    <div className="w-40 h-40 min-w-40 cursor-pointer hover:bg-gray-800  transition-all duration-300 bg-grey-6 mx-2 rounded-xl flex flex-col overflow-hidden justify-between pb-2">
       <img
         src="https://st4.depositphotos.com/2572561/31066/i/450/depositphotos_310665768-stock-photo-over-the-shoulder-shot-of.jpg"
         className="h-1/3 w-full object-cover"
