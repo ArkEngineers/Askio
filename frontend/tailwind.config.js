d0379a29ab9a6b749ae346f27b71580cf304db0d
= <mridultiwari2002@gmail.com>
2024-11-09 15:53:04 +0530

    updated color

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,9 +3,7 @@ const withMT = require("@material-tailwind/react/utils/withMT");
 
 module.exports = withMT({
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    ".src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
