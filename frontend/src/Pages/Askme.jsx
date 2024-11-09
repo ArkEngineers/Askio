a9dcdcddd7edce1b1f35c7e46fc5a6987a6113db
Prashant2002pd <92350732+Prashant2002pd@users.noreply.github.com>
2024-11-09 23:01:30 +0530

    added askme page with fill logic

--- frontend/src/Pages/Askme.jsx
+++ frontend/src/Pages/Askme.jsx
@@ -1,45 +1,188 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
+
 import { Textarea, Button, IconButton } from "@material-tailwind/react";
 import { IoMdSend } from "react-icons/io";
 import { MdCancel } from "react-icons/md";
 import { CgFileAdd } from "react-icons/cg";
 
+const colorTags = [
+  "Blue",
+  "Teal",
+  "Mint",
+  "Green",
+  "Sage",
+  "Yellow",
+  "Beige",
+  "Brown",
+  "Orange",
+  "Peach",
+  "Red",
+];
+
 function Askme() {
   const [text, setText] = useState("");
   const [chat, setChat] = useState([""]);
+  const [modal, setModal] = useState(true);
+  const searchRef = useRef(null);
+  const [selectedTags, setSelectedTags] = useState([]);
 
+  const removeTag = (tag) => {
+    setSelectedTags(selectedTags.filter((t) => t !== tag));
+  };
+  // Detect clicks outside the SearchPalette component
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (searchRef.current && !searchRef.current.contains(event.target)) {
+        setModal(true);
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+  }, []);
   return (
-    <div className="h-screen p-2flex flex-col justify-center items-center">
-      <div className=" border-2 border-base-2 rounded-md min-h-[60%] p-2 text">
+    <div className=" pl-20 pt-24 h-screen p-2 flex flex-col justify-center items-center">
+      <div className=" border-2 w-8/12 border-base-2 rounded-md min-h-[60%] p-2 text">
         {chat.map((item, key) => {
           return (
             <div
               key={key}
               className="bg-grey-9 p-2 rounded-md mb-2 text-white text-right"
             >
-              {console.log(item)}
               <p>{item}</p>
             </div>
           );
         })}
       </div>
-      <div className=" flex h-10 items-center gap-2">
-        <button className="flex justify-center items-center text-center rounded-full bg-base-3 p-1">
-          All
-          <MdCancel />
-        </button>
-        <button className="flex justify-center items-center text-center rounded-full border border-white bg-grey-9 p-1">
-          <CgFileAdd />
-          Add Context
-        </button>
-      </div>
-
-      <div className="">
+      {modal ? (
+        <div className="flex w-8/12 h-10 items-center gap-2 m-2 p-4">
+          {!selectedTags.length && (
+            <button className="flex w-20 px-3 justify-between items-center text-center rounded-full bg-base-3 p-1">
+              All
+              <MdCancel />
+            </button>
+          )}
+          {selectedTags.map((tag) => (
+            <span
+              key={tag}
+              className="bg-gray-200 w-20 justify-between px-3 text-gray-700 rounded-full py-1 mr-2 flex items-center"
+            >
+              {tag}
+              <button
+                className="ml-2 text-xs text-gray-500 hover:text-gray-700"
+                onClick={(e) => {
+                  e.stopPropagation();
+                  removeTag(tag);
+                }}
+              >
+                <MdCancel />
+              </button>
+            </span>
+          ))}
+          <button
+            className="flex w-36 px-3 justify-between items-center text-center rounded-full border border-white bg-grey-9 p-1"
+            onClick={() => setModal(false)}
+          >
+            <CgFileAdd />
+            Add Context
+          </button>
+        </div>
+      ) : (
+        <div className="w-8/12" ref={searchRef}>
+          <SearchPalette
+            selectedTags={selectedTags}
+            setSelectedTags={setSelectedTags}
+            colorTags={colorTags}
+          />
+        </div>
+      )}
+      <div className="w-8/12">
         <Chatarea text={text} setText={setText} chat={chat} setChat={setChat} />
       </div>
     </div>
   );
 }
+
+const SearchPalette = ({ selectedTags, setSelectedTags, colorTags }) => {
+  const [searchInput, setSearchInput] = useState("");
+  const [isDropdownVisible, setDropdownVisible] = useState(false);
+  const dropdownRef = useRef(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setDropdownVisible(false);
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+  }, []);
+
+  const handleInputClick = () => setDropdownVisible(true);
+
+  const handleTagClick = (tag) => {
+    if (!selectedTags.includes(tag)) {
+      setSelectedTags([...selectedTags, tag]);
+    }
+  };
+
+  const removeTag = (tag) => {
+    setSelectedTags(selectedTags.filter((t) => t !== tag));
+  };
+
+  return (
+    <div className="relative w-full" ref={dropdownRef}>
+      <div
+        className="flex flex-wrap items-center rounded-full px-4 py-1 my-1 cursor-pointer"
+        onClick={handleInputClick}
+      >
+        {selectedTags.map((tag) => (
+          <span
+            key={tag}
+            className="bg-gray-200 text-gray-700 rounded-full px-2 py-1 mr-2 flex items-center"
+          >
+            {tag}
+            <button
+              className="ml-2 text-xs text-gray-500 hover:text-gray-700"
+              onClick={(e) => {
+                e.stopPropagation();
+                removeTag(tag);
+              }}
+            >
+              <MdCancel />
+            </button>
+          </span>
+        ))}
+        <input
+          type="text"
+          placeholder="Search palettes"
+          className="border-none outline-none flex-grow bg-transparent ml-2"
+          value={searchInput}
+          onChange={(e) => setSearchInput(e.target.value)}
+          onFocus={handleInputClick}
+          autoFocus
+        />
+      </div>
+
+      {isDropdownVisible && (
+        <div className="absolute top-full left-0 w-full bg-grey-9 border border-base-2 rounded-lg shadow-md mt-2 p-4">
+          <div className="mb-4">
+            <div className="flex flex-wrap mt-2">
+              {colorTags.map((tag) => (
+                <span
+                  key={tag}
+                  className="bg-gray-100 text-gray-600 rounded-full px-3 py-1 m-1 cursor-pointer hover:bg-gray-200"
+                  onClick={() => handleTagClick(tag)}
+                >
+                  {tag}
+                </span>
+              ))}
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
 
 function Chatarea({ text, setText, chat, setChat }) {
   function handleChange(e) {
@@ -57,7 +200,7 @@ function Chatarea({ text, setText, chat, setChat }) {
         rows={1}
         value={text}
         resize={false}
-        placeholder="Your Message"
+        placeholder="Your Contexts"
         className="min-h-full !border-0 focus:border-transparent"
         containerProps={{
           className: "grid h-full",
