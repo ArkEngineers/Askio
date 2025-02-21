466e4daf38b40ddc47c3b9604a96cc834ca3e48c
Prashant2002pd <92350732+Prashant2002pd@users.noreply.github.com>
2025-02-21 18:52:57 +0530

    updated routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,10 @@ import { useEffect, useState } from "react";
 import {
   createBrowserRouter,
   RouterProvider,
-  Navigate
+  Navigate,
+  Routes,
+  Route,
+  BrowserRouter
 } from "react-router-dom";
 import MainLayout from "./Pages/More/MainLayout";
 import LandingPage from "./Pages/LandingPage";
@@ -15,6 +18,9 @@ import Flashcards from "./Pages/Flashcard";
 
 function App() {
   const { user,loggedin,setLoggedIn } = useAuth();
+
+
+  
   useEffect(()=>{
     if (user!=null){
       setLoggedIn(true)
@@ -23,45 +29,24 @@ function App() {
     }
   },[user])
 
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Navigate to={loggedin? "/app" : "/landing"} />,
-    },
-    {
-      path: "/landing",
-      element: loggedin?<Navigate to="/app"/>:<LandingPage />,
-    },
-    {
-      path: "/app",
-      element: loggedin?<MainLayout/>:<Navigate to="/landing"/>,
-      children: [
-        {
-          path: "/app",
-          element: <Home />, // Home must have <Outlet /> to render child components
-        },
-        {
-          path: "/app/flashcard", // child route without leading slash
-          element: <Flashcards />,
-        },
-        {
-          path: "/app/askme", // child route without leading slash
-          element: <Askme />,
-        },
-        {
-          path: "/app/puzzle/:slugs",
-          element: <Quiz />,
-        },
-        {
-          path: "/app/play/:slugs",
-          element: <Play />,
-        },
-      ],
-    },
-  ]);
+ 
   return (
     <div>
-      <RouterProvider router={router} />
+      <>
+      <BrowserRouter>
+      <Routes>
+      <Route path="/" element={<Navigate to={loggedin ? "/app" : "/landing"} />} />
+      <Route path="/landing" element={loggedin ? <Navigate to="/app" /> : <LandingPage />} />
+      <Route path="/app" element={loggedin ? <MainLayout /> : <Navigate to="/landing" />}>
+        <Route path="/app" element={<Home />} />
+        <Route path="/app/flashcard" element={<Flashcards />} />
+        <Route path="/app/askme" element={<Askme />} />
+        <Route path="/app/puzzle/:slugs" element={<Quiz />} />
+        <Route path="/app/play/:slugs" element={<Play />} />
+      </Route>
+    </Routes>
+    </BrowserRouter>
+    </>
     </div>
   );
 }
